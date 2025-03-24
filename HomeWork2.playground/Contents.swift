2cc1f45a3a0c82f1ec98aa8e11f2fa04fa5d13c8
Vitalii Yatsenko <yatsenkovitalikbk@gmail.com>
2025-03-24 14:17:04 +0200

    Done task 1.5

--- HomeWork2.playground/Contents.swift
+++ HomeWork2.playground/Contents.swift
@@ -242,7 +242,11 @@ print("\nРозділ 1.5 Опціонали\n")
 
  */
 
-print("Користувач авторизован: ")
+var userLoggedIn: Bool?
 
-print("Користувач авторизован: ")
+print("Користувач авторизован: \(userLoggedIn) ")
+
+userLoggedIn = true
+
+print("Користувач авторизован: \(userLoggedIn)")
 
